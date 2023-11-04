a372ae9af621132b38e608023041b1b38ec7b9db
Bryan C Guner <bryan.guner@gmail.com>
2023-11-03 20:18:59 -0400

    seperation of concerns

--- 01-python-basics/blockchain.py
+++ 01-python-basics/blockchain.py
@@ -1,8 +1,12 @@
 blockchain = [[1]]
 
 
+def get_last_blockchain_value():
+    return blockchain[-1]
+
+
 def add_value(transaction_amount):
-    blockchain.append([blockchain[-1], transaction_amount])
+    blockchain.append([get_last_blockchain_value(), transaction_amount])
     print(blockchain)
 
 
