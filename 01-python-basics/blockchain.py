5337c372ca4a27e6488ae5938c6b3ba2c5146df7
Bryan C Guner <bryan.guner@gmail.com>
2023-11-04 12:03:54 -0400

    reduced repetatitive code

--- 01-python-basics/blockchain.py
+++ 01-python-basics/blockchain.py
@@ -7,17 +7,20 @@ def get_last_blockchain_value():
 
 def add_value(transaction_amount, last_transaction=[1]):
     blockchain.append([last_transaction, transaction_amount])
+
+def get_user_input():
+    return float(input('Transaction amount?  '))
     
 
-tx_amount = float(input('Transaction amount?  '))
+tx_amount = get_user_input()
 add_value(tx_amount)
 
 
-tx_amount = float(input('Transaction amount?  '))
+tx_amount = get_user_input()
 add_value(last_transaction=get_last_blockchain_value(), transaction_amount=tx_amount)
 
 
-tx_amount = float(input('Transaction amount?  '))
+tx_amount = get_user_input()
 add_value(last_transaction=get_last_blockchain_value(),transaction_amount=tx_amount)
 
 
